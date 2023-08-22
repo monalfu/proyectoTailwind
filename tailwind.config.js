6790b9f51081de0e94ad13022000b24cb2b3256d
Montse Alguacil <monalfu.developer@gmail.com>
2023-08-22 18:27:11 +0200

    Instalación plugin aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
   theme: {
     extend: {},
   },
+  corePlugins: {
+    aspectRatio: false,
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
 
